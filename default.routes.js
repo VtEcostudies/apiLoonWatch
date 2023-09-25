c2754dfe5b544c3df80e4903740acade6c74def9
jloomisVCE <jloomis@vtecostudies.org>
2023-09-25 12:20:36 -0400

    use request host/protocol for links

--- default.routes.js
+++ default.routes.js
@@ -8,9 +8,17 @@ router.get('/', defRes);
 module.exports = router;
 
 function defRes(req, res, next) {
-    res.send(`
+    console.log('hdr.host:', req.headers.host);
+    console.log('protocol:', req.protocol);
+    console.log('hostname:', req.hostname);
+    let html = `
     <p><a href="http://${cfg.api[env.api_env].host}:${cfg.api[env.api_env].port}/loonwatch">LoonWatch Data</a></p>
     <p><a href="http://${cfg.api[env.api_env].host}:${cfg.api[env.api_env].port}/loonwatch/count">LoonWatch Summary</a></p>
-    `);
+    `;
+    html = `
+    <p><a href="${req.protocol}://${req.headers.host}/loonwatch">LoonWatch Data</a></p>
+    <p><a href="${req.protocol}://${req.headers.host}/loonwatch/count">LoonWatch Summary</a></p>
+    `;
+    res.send(html);
 }
 
